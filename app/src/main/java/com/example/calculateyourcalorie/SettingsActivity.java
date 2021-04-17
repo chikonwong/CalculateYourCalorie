7dfca5d8f07b0854f0b524fd6226ff014f221b5d
Jacky Wong <chikonwong@gmail.com>
2021-04-17 23:17:00 +0800

    finish

--- app/src/main/java/com/example/calculateyourcalorie/SettingsActivity.java
+++ app/src/main/java/com/example/calculateyourcalorie/SettingsActivity.java
@@ -4,18 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SettingsActivity extends AppCompatActivity {
+    Button buttonProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings);
         setTitle("Settings");
+        buttonProfile = findViewById(R.id.Button_EditProfile);
+        buttonProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SettingsActivity.this, EditProfileActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
 
-    }
-    public void editProfile() {
-        Intent intent = new Intent(SettingsActivity.this, EditProfileActivity.class);
-        startActivity(intent);
-    }
 }
