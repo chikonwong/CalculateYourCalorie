828a79516257d21a6237406deebc02d23075a2ee
Jacky Wong <chikonwong@gmail.com>
2021-04-23 21:51:58 +0800

    intent email

--- app/src/main/java/com/example/calculateyourcalorie/ContactActivity.java
+++ app/src/main/java/com/example/calculateyourcalorie/ContactActivity.java
@@ -2,13 +2,43 @@ package com.example.calculateyourcalorie;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class ContactActivity extends AppCompatActivity {
+    Button buttonSend;
+    String textTo;
+    EditText textSubject;
+    EditText textMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.contact);
+        setTitle("ContactUs");
+
+        buttonSend = (Button) findViewById(R.id.buttonSend);
+        textTo = "abc@gmail.com";
+        textSubject = (EditText) findViewById(R.id.editTextSubject);
+        textMessage = (EditText) findViewById(R.id.editTextMessage);
+        buttonSend.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                String subject = textSubject.getText().toString();
+                String message = textMessage.getText().toString();
+
+                Intent email = new Intent(Intent.ACTION_SEND);
+                email.putExtra(Intent.EXTRA_EMAIL, new String[]{textTo});
+                email.putExtra(Intent.EXTRA_SUBJECT, subject);
+                email.putExtra(Intent.EXTRA_TEXT, message);
+                email.setType("message/rfc822");
+                startActivity(Intent.createChooser(email, "Choose an Email client :"));
+            }
+        });
     }
 }
--- app/src/main/java/com/example/calculateyourcalorie/SettingsActivity.java
+++ app/src/main/java/com/example/calculateyourcalorie/SettingsActivity.java
@@ -9,6 +9,7 @@ import android.widget.Button;
 
 public class SettingsActivity extends AppCompatActivity {
     Button buttonProfile;
+    Button buttonContect;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,10 +17,18 @@ public class SettingsActivity extends AppCompatActivity {
         setContentView(R.layout.settings);
         setTitle("Settings");
         buttonProfile = findViewById(R.id.Button_EditProfile);
+        buttonContect = findViewById(R.id.Button_ContactUs);
         buttonProfile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(SettingsActivity.this, EditProfileActivity.class);
+                startActivity(intent);
+            }
+        });
+        buttonContect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SettingsActivity.this, ContactActivity.class);
                 startActivity(intent);
             }
         });
